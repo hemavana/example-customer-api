aeaf6b5817762145d32020004ad9080c75020d23
Bhavana <115095929+hemavana@users.noreply.github.com>
2025-02-13 20:55:22 +0000

    Create SpringBootCustomerApiApplication.java

---
+++ src/main/java/com/deloitte/accelerator/springbootcustomerapi/SpringBootCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.accelerator.springbootcustomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomerApiApplication.class, args);
+	}
+
+}
